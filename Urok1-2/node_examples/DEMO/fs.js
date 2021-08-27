f68ad68f32b7d4f317c6586489f36b755dfd77b6
jonyweb20 <jonyweb20@gmail.com>
2021-08-27 22:46:16 +0700

    v.1.5

--- Urok1-2/node_examples/DEMO/fs.js
+++ Urok1-2/node_examples/DEMO/fs.js
@@ -1,24 +1,64 @@
 const fs = require('fs');
 const path = require('path');
 
-const testDR = fs.mkdir(path.join(__dirname, 'testDR', example.txt), (err) => {
-    if(err) throw `Не удалось создать файл ${err}`;
-    else { console.log('Дирректория создана')}// создать исключение (ошибку)
- });
+
+    const testDR = fs.mkdir(path.join(__dirname, 'testDR'), (err) => {
+        try {
+            if (fs.existsSync('testDR')) {
+               console.log('file exists')
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    });
+
+
+const filePathDR = path.join(__dirname, 'testDR', 'exampleText.txt');
+fs.writeFile(filePathDR, 'Первый текст внесенный через Node', err => {
+    if (err) {
+        throw err;
+    }
+    console.log('Текст внесен')
+})
+const filePathDR2 = path.join(__dirname, 'testDR', 'exampleFileText.txt');
+fs.writeFile(filePathDR2, 'Второй текст внесенный через Node', err => {
+    if (err) {
+        throw err;
+    }
+    console.log('Текст внесен')
+})
+
+fs.appendFile(filePathDR, '\nHello n', (err) => {
+    if (err) throw `Не удалось внести текст ${err}`;
+    console.log('Файл создан')
+});
+
+   const data = fs.readFile('exampleFileText.txt', 'utf8', err => {
+        if (err) throw err;
+    });
+    const datafile =  fs.readFile('exampleText.txt', 'utf8', err => {
+            if (err) throw err;
+        });
+
+
+    const filePathDR3 = path.join(__dirname, 'testDR', 'exampleAkkFile.txt');
+    fs.appendFile(filePathDR3, `${data} + \n${datafile}` , err => {
+        if (err) throw `Не удалось внести текст ${err}`;
+        console.log('Файл создан')
+    })
+
+/*
 const filePath = path.join(__dirname, 'test', 'text.txt');
-/*fs.writeFile(filePath, 'Hello Node', (err) => {
+/!*fs.writeFile(filePath, 'Hello Node', (err) => {
     if(err) throw `Не удалось создать файл ${err}`;
     console.log('Файл создан')
-});*/
+});*!/
 fs.appendFile(filePath, '\nHello again', (err) => {
     if (err) throw `Не удалось создать файл ${err}`;
     console.log('Файл создан')
 });
-fs.appendFile(testDR, '\nHello n', (err) => {
-    if (err) throw `Не удалось создать файл ${err}`;
-    console.log('Файл создан')
-});
+
 fs.readFile(filePath, 'utf-8', (err, content)=> {
     if (err) throw `Не удалось прочитать файл ${err}`;
     console.log(content);
-});
+});*/
--- Urok1-2/node_examples/DEMO/path.js
+++ Urok1-2/node_examples/DEMO/path.js
@@ -1,6 +1,7 @@
 const path = require('path');
-console.log(`Название файла ${path.basename(__filename)}`);
-console.log(`Имя директории ${path.dirname(__filename)}`);
+console.log(`Название файла: ${path.basename(__filename, '.js')}`);
+console.log(`Название файла: ${path.basename(__filename)}`);
+console.log(`Имя директории: ${path.dirname(__filename)}`);
 console.log(`Расширение файла: ${path.extname(__filename)}`);
 console.log(`Полная инфа о файле: ${path.parse(__filename)}`);
 console.log(`Объединение: ${path.join(__dirname, 'server', 'index.html')}`);

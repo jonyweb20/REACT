b38062929bfd40d417ba0ce8cd341abe089ded5d
jonyweb20 <jonyweb20@gmail.com>
2021-08-28 17:49:27 +0700

    v.1.6

--- Urok1-2/node_examples/DEMO/fs.js
+++ Urok1-2/node_examples/DEMO/fs.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 
-    const testDR = fs.mkdir(path.join(__dirname, 'testDR'), (err) => {
+    fs.mkdir(path.join(__dirname, 'testDR'), (err) => {
         try {
             if (fs.existsSync('testDR')) {
                console.log('file exists')
@@ -14,38 +14,88 @@ const path = require('path');
 
 
 const filePathDR = path.join(__dirname, 'testDR', 'exampleText.txt');
-fs.writeFile(filePathDR, 'Первый текст внесенный через Node', err => {
+fs.writeFile(filePathDR, 'Первый текст внесенный посредством Node ', err => {
     if (err) {
         throw err;
     }
-    console.log('Текст внесен')
+    console.log('Текст первый внесен')
 })
 const filePathDR2 = path.join(__dirname, 'testDR', 'exampleFileText.txt');
-fs.writeFile(filePathDR2, 'Второй текст внесенный через Node', err => {
+fs.writeFile(filePathDR2, 'Второй текст внесенный через Node ', err => {
     if (err) {
         throw err;
     }
-    console.log('Текст внесен')
+    console.log('Текст второй внесен')
 })
-
 fs.appendFile(filePathDR, '\nHello n', (err) => {
     if (err) throw `Не удалось внести текст ${err}`;
-    console.log('Файл создан')
+    console.log('Файл третий внесен')
 });
 
-   const data = fs.readFile('exampleFileText.txt', 'utf8', err => {
-        if (err) throw err;
-    });
-    const datafile =  fs.readFile('exampleText.txt', 'utf8', err => {
+
+const filePathDR3 = path.join(__dirname, 'testDR', 'exampleAkkFile.txt');
+console.log('Файл exampleAkkFile создан')
+   fs.readFile(filePathDR2, 'utf8',(err, data) => {
+       if (err) throw err;
+       console.log('Файл exampleFileText считан')
+       fs.writeFile(filePathDR3, `${data}`, err => {
+           if (err) throw `Не удалось внести текст ${err}`;
+           console.log('Файл exampleFileText внесен в exampleAkkFile')
+       })
+       fs.readFile(filePathDR, 'utf8',(err, data) => {
+           if (err) throw err;
+           fs.appendFile(filePathDR3, `\n${data}`, err => {
+               if (err) throw `Не удалось внести текст ${err}`;
+               console.log('Файл exampleText внесен в exampleAkkFile')
+           })
+       });
+   });
+
+
+const filePathDR4 = path.join(__dirname, 'testDR', 'exampleEndFile.txt');
+console.log('Файл exampleEndFile создан')
+fs.readFile(filePathDR3, 'utf8', (err, text) => {
+    let arrWord = text.split(' ');
+let newArrWord = [...new Set(arrWord)]
+    fs.writeFile(filePathDR4, `${newArrWord}`, err => {
+        if (err) throw `Не удалось внести текст ${err}`;
+        console.log('Данные в exampleEndFile занесены')
+    })
+} )
+
+/*fs.unlink(filePathDR, err=>{
+    if (err) throw `Не удалось удалить файл ${err}`;
+    console.log('Файл exampleText удален')
+})
+fs.unlink(filePathDR2, err=>{
+    if (err) throw `Не удалось удалить файл ${err}`;
+    console.log('Файл exampleFileText удален')
+})
+fs.unlink(filePathDR3, err=>{
+    if (err) throw `Не удалось удалить файл ${err}`;
+    console.log('Файл exampleAkkFile удален')
+})*/
+
+
+
+
+
+
+
+
+
+
+/*const datafile =  fs.readFile(filePathDR, 'utf8', (err, data) => {
             if (err) throw err;
-        });
+return data
+        });*/
 
 
-    const filePathDR3 = path.join(__dirname, 'testDR', 'exampleAkkFile.txt');
-    fs.appendFile(filePathDR3, `${data} + \n${datafile}` , err => {
+  /* const filePathDR3 = path.join(__dirname, 'testDR', 'exampleAkkFile.txt');
+    fs.writeFile(filePathDR3, `${dataf} ` , err => {
         if (err) throw `Не удалось внести текст ${err}`;
         console.log('Файл создан')
-    })
+    })*/
 
 /*
 const filePath = path.join(__dirname, 'test', 'text.txt');
